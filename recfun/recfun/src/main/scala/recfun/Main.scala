1642eaa18f3f76a4fe48e34ed1be51bbeaaad9b3
unknown <tyler.sammann@TSAMMANN-T430.corp.natpal.com>
2014-05-04 01:00:55 -0500

    fixed for 100% on coursera

--- recfun/recfun/src/main/scala/recfun/Main.scala
+++ recfun/recfun/src/main/scala/recfun/Main.scala
@@ -16,9 +16,10 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c < 0 || c > r) return 0
-    if (r == 0) return 1
-    pascal(c-1, r-1) + pascal(c,r-1)
+    if (c < 0 || c > r) 0
+    else if (r == 0) 1
+    else pascal(c-1, r-1) + pascal(c,r-1)
+
   }
 
   /**
@@ -26,17 +27,11 @@ object Main {
    */
   def balance(chars: List[Char]): Boolean = {
     def loop(chars: List[Char], level: Int): Boolean = {
-      if (level < 0) return false
-      if (chars.isEmpty) {
-        if (level == 0) {
-          return true
-        } else {
-          return false
-        }
-      }
-      if (chars.head == '(') return loop(chars.tail, level+1)
-      if (chars.head == ')') return loop(chars.tail, level-1)
-      loop(chars.tail, level)
+      if (level < 0 || (chars.isEmpty && level > 0)) false
+      else if (chars.isEmpty && level == 0) true
+      else if (chars.head == '(') loop(chars.tail, level+1)
+      else if (chars.head == ')') loop(chars.tail, level-1)
+      else loop(chars.tail, level)
     }
     loop(chars, 0)
   }
@@ -44,15 +39,15 @@ object Main {
   /**
    * Exercise 3
    */
-  def count(cents: Int, coins: List[Int], total: Int): Int = {
+  def count(money: Int, coins: List[Int], total: Int): Int = {
     if (coins.isEmpty)
-      return total
-    else if (cents - coins.head == 0)
-      return count(cents, coins.tail, total+1)
-    else if (cents - coins.head < 0)
-      return count(cents, coins.tail, total)
-
-    count(cents - coins.head, coins, total) + countChange(cents, coins.tail)
+      total
+    else if (money - coins.head == 0)
+      count(money, coins.tail, total+1)
+    else if (money - coins.head < 0)
+      count(money, coins.tail, total)
+    else
+      count(money - coins.head, coins, total) + countChange(money, coins.tail)
   }
 
   def countChange(money: Int, coins: List[Int]): Int = {
